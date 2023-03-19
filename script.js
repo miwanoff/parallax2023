a79cfb14f4deb754651da91ba450c7278b69fa3a
miwanoff <marina.iva.noffa@gmail.com>
2023-03-19 12:43:41 +0200

    Add parallax

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+  $(window).scroll(function () {
+    /* Скроллировать фоновые слои с разной скоростью */
+    let winTop = $(window).scrollTop();
+    $(".parallax").css(
+      "background-position",
+      "center " + (-120 -(winTop * 0.5)) + "px"
+    );
+  });
+});
